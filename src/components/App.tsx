b7bcf10e2295aed779fe8b662e8c351150919552
Lorenzo Sicilia <lorenzo.sicilia@gmail.com>
2020-05-24 15:36:34 +0100

    Show Ether balance

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,11 @@ import { Web3ReactProvider } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
 import { useWeb3React } from '@web3-react/core'
 import { InjectedConnector } from '@web3-react/injected-connector'
+import useSWR from 'swr'
+import {formatEther} from "@ethersproject/units";
+
+export const shorter = str =>
+    str?.length > 8 ? str.slice(0, 6) + '...' + str.slice(-4) : str
 
 export const injectedConnector = new InjectedConnector({
   supportedChainIds: [
@@ -20,6 +25,24 @@ function getLibrary(provider: any): Web3Provider {
   return library
 }
 
+const fetcher = (library) => (...args) => {
+  const [method, ...params] = args
+  console.log(method, params)
+  return library[method](...params)
+}
+
+export const Balance = () => {
+  const { account, library } = useWeb3React<Web3Provider>()
+  const { data: balance } = useSWR(['getBalance', account, 'latest'], {
+    fetcher: fetcher(library),
+  })
+  if(!balance) {
+    return <div>...</div>
+  }
+  return <div>Ξ {parseFloat(formatEther(balance)).toPrecision(4)}</div>
+}
+
+
 export const Wallet = () => {
   const { chainId, account, activate, active } = useWeb3React<Web3Provider>()
 
@@ -30,7 +53,7 @@ export const Wallet = () => {
   return (
     <div>
       <div>ChainId: {chainId}</div>
-      <div>Account: {account}</div>
+      <div>Account: {shorter(account)}</div>
       {active ? (
         <div>✅ </div>
       ) : (
@@ -38,6 +61,7 @@ export const Wallet = () => {
           Connect
         </button>
       )}
+      {active && <Balance />}
     </div>
   )
 }
