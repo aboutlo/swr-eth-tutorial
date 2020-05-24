85567174973e5a17bbbc45726ed014c638d05c5e
Lorenzo Sicilia <lorenzo.sicilia@gmail.com>
2020-05-24 11:35:58 +0100

    Connect to the network

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,68 +1,51 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import {
-  Web3ReactProvider,
-  useWeb3React,
-  UnsupportedChainIdError,
-  createWeb3ReactRoot,
-} from "@web3-react/core";
-import { Web3Provider } from "@ethersproject/providers";
-import useSWR, { SWRConfig } from "swr";
-import { createWatcher, aggregate } from "@makerdao/multicall";
-import { Wallet } from "./Wallet";
+import React from 'react'
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import { useWeb3React } from '@web3-react/core'
+import { InjectedConnector } from '@web3-react/injected-connector'
+
+export const injectedConnector = new InjectedConnector({
+  supportedChainIds: [
+    1, // Mainet
+    3, // Ropsten
+    4, // Rinkeby
+    5, // Goerli
+    42, // Kovan
+  ],
+})
 
 function getLibrary(provider: any): Web3Provider {
-  console.log({ provider });
-  const library = new Web3Provider(provider);
-  library.pollingInterval = 12000;
-  return library;
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
 }
 
-// Contract addresses used in this example
-const MKR_TOKEN = "0xaaf64bfcc32d0f15873a02163e7e500671a4ffcd";
-const MKR_WHALE = "0xdb33dfd3d61308c33c63209845dad3e6bfb2c674";
-const MKR_FISH = "0x2dfcedcb401557354d0cf174876ab17bfd6f4efd";
+export const Wallet = () => {
+  const { chainId, account, activate, active } = useWeb3React<Web3Provider>()
 
-// const config = { preset: "rinkeby" };
+  const onClick = () => {
+    activate(injectedConnector)
+  }
 
-const Container = styled.div`
-  border: 1px solid #ccc;
-  font-family: monospace;
-  white-space: pre;
-`;
-
-// Create watcher
-/*const watcher = createWatcher(
-  [
-    {
-      target: MKR_TOKEN,
-      call: ["balanceOf(address)(uint256)", MKR_WHALE],
-      returns: [["BALANCE_OF_MKR_WHALE", (val) => val / 10 ** 18]],
-    },
-  ],
-  config
-);
-
-// Subscribe to state updates
-watcher.subscribe((update) => {
-  console.log(`Update: ${update.type} = ${update.value}`);
-});
-
-// Subscribe to batched state updates
-watcher.batch().subscribe((updates) => {
-  // Handle batched updates here
-  // Updates are returned as { type, value } objects, e.g:
-  // { type: 'BALANCE_OF_MKR_WHALE', value: 70000 }
-});
-
-// Subscribe to new block number updates
-watcher.onNewBlock((blockNumber) => {
-  console.log("New block:", blockNumber);
-});*/
-
-// Start the watcher polling
-/*watcher.start();*/
+  return (
+    <div>
+      <div>ChainId: {chainId}</div>
+      <div>Account: {account}</div>
+      {active ? (
+        <div>✅ </div>
+      ) : (
+        <button type="button" onClick={onClick}>
+          Connect
+        </button>
+      )}
+    </div>
+  )
+}
 
 export const App = () => {
-  return <Web3ReactProvider getLibrary={getLibrary} children={<Wallet />} />;
-};
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Wallet />
+    </Web3ReactProvider>
+  )
+}
--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,45 +1,21 @@
-import React, { Suspense } from "react"
-import { SWRConfig } from "swr"
-import { useWeb3React } from "@web3-react/core"
-import { Web3Provider } from "@ethersproject/providers"
+import React from 'react'
+import { useWeb3React } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
 
-import { useEagerConnect } from "../hooks/useEagerConnect"
-import { useInactiveListener } from "../hooks/useInactiveListener"
-import { DisplayInfo } from "./DisplayInfo"
-import { web3Fetcher } from "../utils"
-import { injected } from "../connectors"
-import { ABIs } from "../abi"
+import { injected } from '../connectors'
 
 export const Wallet = () => {
-  const context = useWeb3React<Web3Provider>()
   const {
-    connector,
-    library,
     chainId,
     account,
     activate,
-    deactivate,
     active,
-    error,
-  } = context
+  } = useWeb3React<Web3Provider>()
 
   const onClick = () => {
     activate(injected)
   }
 
-  // handle logic to recognize the connector currently being activated
-  const [activatingConnector, setActivatingConnector] = React.useState()
-  React.useEffect(() => {
-    console.log("Wallet running")
-    if (activatingConnector && activatingConnector === connector) {
-      setActivatingConnector(undefined)
-    }
-  }, [activatingConnector, connector])
-
-  // mount only once or face issues :P
-  const triedEager = useEagerConnect()
-  useInactiveListener(!triedEager || !!activatingConnector)
-  console.log({ library, active, triedEager, activatingConnector })
   return (
     <div>
       <div>ChainId: {chainId}</div>
@@ -50,17 +26,6 @@ export const Wallet = () => {
         <button type="button" onClick={onClick}>
           Connect
         </button>
-      )}
-      {library && (
-        <SWRConfig
-          value={{
-            fetcher: web3Fetcher(library, ABIs),
-            suspense: true,
-            focusThrottleInterval: 5000,
-          }}
-        >
-          <DisplayInfo chainId={chainId} />
-        </SWRConfig>
       )}
     </div>
   )
